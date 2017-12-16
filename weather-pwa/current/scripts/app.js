84cdc96611529bf06c027db9179ded328a6c2324
Safwan Samla <safwans@gmail.com>
2017-12-15 20:17:15 -0500

    update

--- weather-pwa/current/scripts/app.js
+++ weather-pwa/current/scripts/app.js
@@ -2,6 +2,33 @@
 (function() {
   'use strict';
 
+  var injectedForecast = {
+      key: 'newyork',
+      label: 'New York, NY',
+      currently: {
+        time: 1453489481,
+        summary: 'Clear',
+        icon: 'partly-cloudy-day',
+        temperature: 52.74,
+        apparentTemperature: 74.34,
+        precipProbability: 0.20,
+        humidity: 0.77,
+        windBearing: 125,
+        windSpeed: 1.52
+      },
+      daily: {
+        data: [
+          {icon: 'clear-day', temperatureMax: 55, temperatureMin: 34},
+          {icon: 'rain', temperatureMax: 55, temperatureMin: 34},
+          {icon: 'snow', temperatureMax: 55, temperatureMin: 34},
+          {icon: 'sleet', temperatureMax: 55, temperatureMin: 34},
+          {icon: 'fog', temperatureMax: 55, temperatureMin: 34},
+          {icon: 'wind', temperatureMax: 55, temperatureMin: 34},
+          {icon: 'partly-cloudy-day', temperatureMax: 55, temperatureMin: 34}
+        ]
+      }
+    };
+
   var weatherAPIUrlBase = 'https://publicdata-weather.firebaseio.com/';
 
   var app = {
@@ -41,6 +68,7 @@
     var label = selected.textContent;
     app.getForecast(key, label);
     app.selectedCities.push({key: key, label: label});
+    app.saveSelectedCities();
     app.toggleAddDialog(false);
   });
 
@@ -149,5 +177,26 @@
       app.getForecast(key);
     });
   };
-
+  
+  app.saveSelectedCities = function() {
+      window.localforage.setItem('selectedCities', app.selectedCities);
+  };
+  
+  document.addEventListener('DOMContentLoaded', function() {
+      window.localforage.getItem('selectedCities', function(err, cityList) {
+          if (cityList) {
+              app.selectedCities = cityList;
+              app.selectedCities.forEach(function(city) {
+                  app.getForecast(city.key, city.label);
+              });
+          } else {
+              app.updateForecastCard(injectedForecast);
+              app.selectedCities = [
+                  {key: injectedForecast.key, label:injectedForecast.label}
+              ];
+              app.saveSelectedCities();
+          }
+      });
+  });  
+  
 })();

753a97c8a1c2750f9ef3aea83331872e7d5b0555
safwans <safwans@dhcp-100-76-25-224.nyc.corp.google.com>
2018-10-04 11:52:41 -0400

    ss

---
+++ clients/nascar/sw.backup.js
@@ -0,0 +1,31 @@
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.6.1/workbox-sw.js');
+
+workbox.precaching.precache([
+  'https://m.nascar.com/wp-content/uploads/sites/7/2017/12/NASCAR_Barmark_Logo-1.svg',  
+  'https://m.nascar.com/wp-content/themes/ndms-2016/images/loading-gif3.gif',
+  'https://m.nascar.com/wp-content/themes/ndms-2016/images/lazy-placeholder.jpg',
+  'https://m.nascar.com/wp-content/themes/ndms-2016/style.css?ver=1.145',
+  'https://m.nascar.com/wp-content/themes/ndms-2016/css/custom-nascar.css',
+  'https://m.nascar.com/wp-content/themes/ndms-2016/fonts/stainless/Stainless-Regular/Stainless-Regular.woff2',
+  'https://m.nascar.com/wp-content/themes/ndms-2016/fonts/stainless/Stainless-Bold/Stainless-Bold.woff2',
+  'https://m.nascar.com/wp-content/themes/ndms-2016/fonts/stainless/Stainless-Black/Stainless-Black.woff2',
+  'offline.html',
+  'https://m.nascar.com/monster-energy-nascar-cup-series/2018/schedule/'
+]);
+
+workbox.routing.registerRoute(
+    new RegExp('.*\/wp-content\/.*\.(?:css|jpg|woff2)'),
+    workbox.strategies.staleWhileRevalidate()
+);
+
+const htmlHandler = workbox.strategies.networkOnly();
+
+// A NavigationRoute matches navigation requests in the browser, i.e. requests for HTML.
+const navigationRoute = new workbox.routing.NavigationRoute(({event}) => {
+  return htmlHandler.handle({event}).catch(() => caches.match('./offline.html'));
+});
+
+workbox.routing.registerRoute(navigationRoute);
+
+workbox.skipWaiting();
+workbox.clientsClaim();
---
+++ clients/nascar/sw.js
@@ -0,0 +1,65 @@
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.6.1/workbox-sw.js');
+
+workbox.precaching.precache([
+  'https://m.nascar.com/wp-content/uploads/sites/7/2017/12/NASCAR_Barmark_Logo-1.svg',  
+  'https://m.nascar.com/wp-content/themes/ndms-2016/images/loading-gif3.gif',
+  'https://m.nascar.com/wp-content/themes/ndms-2016/images/lazy-placeholder.jpg',
+  'https://m.nascar.com/wp-content/themes/ndms-2016/style.css?ver=1.145',
+  'https://m.nascar.com/wp-content/themes/ndms-2016/css/custom-nascar.css',
+  'https://m.nascar.com/wp-content/themes/ndms-2016/fonts/stainless/Stainless-Regular/Stainless-Regular.woff2',
+  'https://m.nascar.com/wp-content/themes/ndms-2016/fonts/stainless/Stainless-Bold/Stainless-Bold.woff2',
+  'https://m.nascar.com/wp-content/themes/ndms-2016/fonts/stainless/Stainless-Black/Stainless-Black.woff2',
+  'offline.html',
+  'https://m.nascar.com/monster-energy-nascar-cup-series/2018/schedule/'
+]);
+
+//A NavigationRoute matches navigation requests in the browser, i.e. requests for HTML.
+const navigationRoute = new workbox.routing.NavigationRoute(({event}) => {
+  return htmlHandler.handle({event}).catch(() => caches.match('./offline.html'));
+});
+workbox.routing.registerRoute(navigationRoute);
+
+
+// Route for images
+workbox.routing.registerRoute(
+    new RegExp('.*\/wp-content\/.*\.(?:css|js)'),
+    workbox.strategies.staleWhileRevalidate({
+    })
+);
+
+// Routing for images
+workbox.routing.registerRoute(
+    new RegExp('.*\/wp-content\/.*\.(?:jpg|jpeg|svg|gif|png)'),
+    workbox.strategies.cacheFirst({
+      cacheName: 'images',
+      plugins: [
+        new workbox.expiration.Plugin({
+          maxEntries: 60,
+          maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
+        }),
+      ],
+    }),
+);
+
+// Routing for fonts
+workbox.routing.registerRoute(
+    new RegExp('.*\/wp-content\/.*\.woff2'),
+    workbox.strategies.cacheFirst({
+      cacheName: 'static-resources',
+      plugins: [
+        new workbox.cacheableResponse.Plugin({
+          statuses: [0, 200],
+        }),
+        new workbox.expiration.Plugin({
+          maxAgeSeconds: 60 * 60 * 24 * 365,
+          maxEntries: 30,
+        }),
+      ],
+    }),
+  );
+
+const htmlHandler = workbox.strategies.networkOnly();
+
+
+workbox.skipWaiting();
+workbox.clientsClaim();

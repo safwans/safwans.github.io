8ae41407c71dac84ca066607531325b7de5e3d97
safwans <safwans@dhcp-100-76-25-224.nyc.corp.google.com>
2018-10-04 12:46:18 -0400

    ss

--- clients/nascar/sw2.js
+++ clients/nascar/sw2.js
@@ -1,5 +1,10 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.6.1/workbox-sw.js');
-
+workbox.core.setCacheNameDetails({
+  prefix: '',
+  suffix: '',
+  precache: 'nascar-cache',
+  runtime: 'nascar-cache'
+});
 workbox.precaching.precache([
   'https://m.nascar.com/wp-content/uploads/sites/7/2017/12/NASCAR_Barmark_Logo-1.svg',  
   'https://m.nascar.com/wp-content/themes/ndms-2016/images/loading-gif3.gif',
@@ -13,19 +18,35 @@ workbox.precaching.precache([
   'https://m.nascar.com/monster-energy-nascar-cup-series/2018/schedule/'
 ]);
 
-workbox.routing.registerRoute(
-    new RegExp('.*\/wp-content\/.*\.(?:css|jpg|woff2)'),
-    workbox.strategies.staleWhileRevalidate()
-);
-
-const htmlHandler = workbox.strategies.networkOnly();
-
-// A NavigationRoute matches navigation requests in the browser, i.e. requests for HTML.
+//A NavigationRoute matches navigation requests in the browser, i.e. requests for HTML.
 const navigationRoute = new workbox.routing.NavigationRoute(({event}) => {
   return htmlHandler.handle({event}).catch(() => caches.match('./offline.html'));
 });
-
 workbox.routing.registerRoute(navigationRoute);
 
+
+// Route for static files
+workbox.routing.registerRoute(
+    new RegExp('.*\/wp-content\/.*\.(?:css|js)'),
+    workbox.strategies.staleWhileRevalidate({
+      cacheName: 'nascar-cache'
+    })
+);
+
+// Routing for images
+workbox.routing.registerRoute(
+    new RegExp('.*\/wp-content\/.*\.(?:jpg|jpeg|svg|gif|png)'),
+    workbox.strategies.cacheFirst({
+      cacheName: 'nascar-cache'
+    })
+);
+
+// Routing for fonts
+workbox.routing.registerRoute(
+    new RegExp('.*\/wp-content\/.*\.woff2'),
+    workbox.strategies.cacheFirst()
+  );
+
+const htmlHandler = workbox.strategies.networkOnly();
 workbox.skipWaiting();
 workbox.clientsClaim();

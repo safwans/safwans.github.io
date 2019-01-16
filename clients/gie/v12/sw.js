0d52e84c53197ae3faf3778edefd0fb0719a3f58
safwans <safwans@100.81.115.101>
2019-01-16 15:48:59 -0500

    ss

--- clients/gie/v12/sw.js
+++ clients/gie/v12/sw.js
@@ -1,9 +1,15 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.6.1/workbox-sw.js');
 
 workbox.precaching.precache([
-  'https://d21gpk1vhmjuf5.cloudfront.net/globalIndustrial-autosuggest.css',
+  'https://d21gpk1vhmjuf5.cloudfront.net/globalIndustrial-autosuggest.css.gz',
   'offline.html'
 ]);
+
+workbox.routing.registerRoute(
+    new RegExp('.*\.(?:jpg|gif|svg|png|woff2).*'),
+    workbox.strategies.staleWhileRevalidate()
+);
+
 
 const htmlHandler = workbox.strategies.networkOnly();
 
@@ -11,10 +17,6 @@ const navigationRoute = new workbox.routing.NavigationRoute(({event}) => {
   return htmlHandler.handle({event}).catch(() => caches.match('./offline.html'));
 });
 
-workbox.routing.registerRoute(
-    new RegExp('.*\.(?:jpg|gif|svg|png|woff2).*'),
-    workbox.strategies.staleWhileRevalidate()
-);
 
 
 workbox.skipWaiting();

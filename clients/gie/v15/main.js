56c9f81880318d2927c4f1a96f4c6a4a972a73b5
safwans <safwans@100.81.115.101>
2019-01-16 18:22:21 -0500

    ss

--- clients/gie/v15/main.js
+++ clients/gie/v15/main.js
@@ -12,3 +12,37 @@ if ('serviceWorker' in navigator) {
     });
   });
 }
+
+const pushButton = document.querySelector('#pushbutton');
+
+
+const applicationServerPublicKey = 'BMB0RqMQM5IV1qOV43SoBnK03P_XWmZbNmUTh3m8T6uECKga-GFVPGFZC4ZZRyG1HWIa6M4T-4aUNwzpLLW3Sz4';
+
+let isSubscribed = false;
+
+function urlB64ToUint8Array(base64String) {
+  const padding = '='.repeat((4 - base64String.length % 4) % 4);
+  const base64 = (base64String + padding)
+    .replace(/\-/g, '+')
+    .replace(/_/g, '/');
+
+  const rawData = window.atob(base64);
+  const outputArray = new Uint8Array(rawData.length);
+
+  for (let i = 0; i < rawData.length; ++i) {
+    outputArray[i] = rawData.charCodeAt(i);
+  }
+  return outputArray;
+}
+
+function updateSubscriptionOnServer(subscription) {
+  // TODO: Send subscription to application server
+
+  const subscriptionJson = document.querySelector('.js-subscription-json');
+  const subscriptionDetails =
+    document.querySelector('.js-subscription-details');
+
+  if (subscription) {
+    subscriptionJson.textContent = JSON.stringify(subscription);
+  }
+}

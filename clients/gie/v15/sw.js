08befb69bd5637aa6233e71d7df55d7c95a28ef6
safwans <safwans@100.81.115.101>
2019-01-17 11:15:23 -0500

    ss

--- clients/gie/v15/sw.js
+++ clients/gie/v15/sw.js
@@ -26,5 +26,30 @@ return htmlHandler.handle({event}).catch(() => caches.match('./offline.html'));
 workbox.routing.registerRoute(navigationRoute);
 
 
+self.addEventListener('push', function(event) {
+  console.log('[Service Worker] Push Received.');
+  console.log(`[Service Worker] Push had this data: "${event.data.text()}"`);
+
+  const title = 'Push from Heaven';
+  const options = {
+    body: 'It\'s beautiful out here',
+    icon: 'images/icon.png',
+    badge: 'images/badge.png'
+  };
+
+  event.waitUntil(self.registration.showNotification(title, options));
+});
+
+self.addEventListener('notificationclick', function(event) {
+  console.log('[Service Worker] Notification click Received.');
+
+  event.notification.close();
+
+  event.waitUntil(
+    clients.openWindow('https://developers.google.com/web/')
+  );
+});
+
+
 workbox.skipWaiting();
 workbox.clientsClaim();
